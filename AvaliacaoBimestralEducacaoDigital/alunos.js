dcb855c0f8c1798534ac73f5fa0cc1f888edf535
Flávio Ambrósio <flavioambrosio@gmail.com>
2026-07-03 17:23:41 -0300

    config: conecta rosterApiUrl da lista protegida

--- AvaliacaoBimestralEducacaoDigital/alunos.js
+++ AvaliacaoBimestralEducacaoDigital/alunos.js
@@ -627,6 +627,8 @@ const STUDENT_DATABASE = {
         "enabled": true,
         "salt": "EDU-DIGITAL-2026",
         "passwordHash": "f267aa257c7116e591f638a9bb704f8c11940f3798b59f7a8f1f6a55d0877be1",
-        "hint": "Solicite ao professor a senha de acesso do estudante."
+        "hint": "Solicite ao professor a senha de acesso do estudante.",
+        "rosterApiUrl": "https://script.google.com/macros/s/AKfycbye5ZFZ95mUfkdUD_iZvFEvHUPww7-t_dKZQaDtvC72PqJhJtdPLs3FHeNFG6SfztXlVQ/exec",
+        "apiTimeoutMs": 6000
     }
 };
