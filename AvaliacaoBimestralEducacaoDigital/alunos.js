32beddcd4802cabdfb07a0aab663eef83bee8c66
Flávio Ambrósio <flavioambrosio@gmail.com>
2026-07-10 18:48:21 -0300

    Sincroniza hash de acesso com API de turmas

--- AvaliacaoBimestralEducacaoDigital/alunos.js
+++ AvaliacaoBimestralEducacaoDigital/alunos.js
@@ -626,7 +626,7 @@ const STUDENT_DATABASE = {
     "accessControl": {
         "enabled": true,
         "salt": "EDU-DIGITAL-2026",
-        "passwordHash": "0e71f1560760cf5cc90d84c76c5028354b4d2366d8841144d34b1fa4b6dacb60",
+        "passwordHash": "f267aa257c7116e591f638a9bb704f8c11940f3798b59f7a8f1f6a55d0877be1",
         "hint": "Solicite ao professor a senha de acesso do estudante."
     }
 };
--- _shared/student-access-config.js
+++ _shared/student-access-config.js
@@ -1,7 +1,7 @@
 window.SIMULATION_ENHANCER_STUDENT_ACCESS = {
     "enabled": true,
     "salt": "EDU-DIGITAL-2026",
-    "passwordHash": "0e71f1560760cf5cc90d84c76c5028354b4d2366d8841144d34b1fa4b6dacb60",
+    "passwordHash": "f267aa257c7116e591f638a9bb704f8c11940f3798b59f7a8f1f6a55d0877be1",
     "hint": "Solicite ao professor a senha de acesso do estudante.",
     "rosterApiUrl": "https://script.google.com/macros/s/AKfycbye5ZFZ95mUfkdUD_iZvFEvHUPww7-t_dKZQaDtvC72PqJhJtdPLs3FHeNFG6SfztXlVQ/exec",
     "apiTimeoutMs": 20000,
