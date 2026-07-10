fd9db964e6bdba4b1fa46f80dd582b044d94592d
Flávio Ambrósio <flavioambrosio@gmail.com>
2026-07-10 19:04:33 -0300

    Alinha hash padrao do enhancer com senha atual

--- _shared/simulation-enhancer.js
+++ _shared/simulation-enhancer.js
@@ -37,7 +37,7 @@
     const DEFAULT_STUDENT_ACCESS_CONFIG = {
         enabled: true,
         salt: 'EDU-DIGITAL-2026',
-        passwordHash: 'f267aa257c7116e591f638a9bb704f8c11940f3798b59f7a8f1f6a55d0877be1',
+        passwordHash: '0e71f1560760cf5cc90d84c76c5028354b4d2366d8841144d34b1fa4b6dacb60',
         hint: 'Solicite ao professor a senha de acesso do estudante.',
         rosterApiUrl: 'https://script.google.com/macros/s/AKfycbye5ZFZ95mUfkdUD_iZvFEvHUPww7-t_dKZQaDtvC72PqJhJtdPLs3FHeNFG6SfztXlVQ/exec',
         apiTimeoutMs: 20000,
