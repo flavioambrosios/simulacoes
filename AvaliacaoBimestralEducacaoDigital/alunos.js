35ca2075518d2bafab6b7bebf96746ded0517fb5
flavioambrosios <flavioambrosio@gmail.com>
2026-07-11 13:49:28 -0300

    Update and rename alunos-copia.js to alunos.js

---
+++ AvaliacaoBimestralEducacaoDigital/alunos.js
@@ -0,0 +1,84 @@
+const STUDENT_DATABASE = {
+    "bySheet": {
+        "2o ano A": [
+            ""
+        ],
+        "2o ano B": [
+            ""
+        ],
+        "3o ano A": [
+            ""
+        ],
+        "3o ano B": [
+            ""
+        ],
+        "3o ano C": [
+            ""
+        ],
+        "3o ano D": [
+            ""
+        ],
+        "3o ano G": [
+            ""
+        ],
+        "PCA - Educação Digital 3o ano E": [
+            ""
+        ],
+        "PCA - Educação Digital 3o ano G": [
+            ""
+        ],
+        "Sustentabilidade 2o ano C": [
+            ""
+        ]
+    },
+    "bySerieTurma": {
+        "2o ano|A": [
+            ""
+        ],
+        "2o ano|B": [
+            ""
+        ],
+        "3o ano|A": [
+            ""
+        ],
+        "3o ano|B": [
+            ""
+        ],
+        "3o ano|C": [
+            ""
+        ],
+        "3o ano|D": [
+            ""
+        ],
+        "3o ano|G": [
+            ""
+        ],
+        "3o ano|E": [
+            ""
+        ],
+        "2o ano|C": [
+            ""
+        ]
+    },
+    "byTrilha": {
+        "PCA - Educação Digital 3o ano E": [
+            ""
+        ],
+        "PCA - Educação Digital 3o ano G": [
+            ""
+        ],
+        "Sustentabilidade 2o ano C": [
+            ""
+        ]
+    },
+    "accessControl": {
+        "enabled": true,
+        "salt": "EDU-DIGITAL-2026",
+        "passwordHash": "0e71f1560760cf5cc90d84c76c5028354b4d2366d8841144d34b1fa4b6dacb60",
+        "hint": "Solicite ao professor a senha de acesso do estudante."
+    }
+};
+
+if (typeof window !== 'undefined') {
+    window.STUDENT_DATABASE = STUDENT_DATABASE;
+}
